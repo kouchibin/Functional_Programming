eba4b0b09d67e0bc5a46f4d8f87ba3b7a7c297d0
Chibin Kou <kouchibin@gmail.com>
2019-12-16 17:45:04 +0100

    Last commit, hopefully.

--- lab4/Calculator.hs
+++ lab4/Calculator.hs
@@ -1,7 +1,3 @@
-
--- The zoom function has been worked into the other functions --
-
-
 import ThreepennyPages
 import Graphics.UI.Threepenny.Core as UI
 import qualified Graphics.UI.Threepenny as UI
@@ -20,15 +16,20 @@ setup window =
      fx             <- mkHTML "<i>f</i>(<i>x</i>)="  -- The text "f(x)="
      input          <- mkInput 20 "x"                -- The formula input
      draw           <- mkButton "Draw graph"         -- The draw button
-     zoomFactor     <- mkHTML "<i>zoom</i>="         -- The text "zoom="
+     zoomLabel      <- mkHTML "<i>Zoom factor</i>="  -- The text "Zoom factor="
      zoomInput      <- mkInput 20 "1"                -- The zoom input, initially 1
-     zoom           <- mkButton "zoom"               -- The zoom button
+     zoomButton     <- mkButton "zoom"               -- The zoom button
      differentiate  <- mkButton "differentiate"      -- The differentiate button
 
      -- Add the user interface elements to the page, creating a specific layout
      formula <- row [pure fx,pure input]
-     zoomFormula <- row [pure zoomFactor, pure zoomInput]
-     getBody window #+ [column [pure canvas,pure formula,pure draw, pure zoomFormula, pure zoom, pure differentiate]]
+     zoom    <- row [pure zoomLabel, pure zoomInput]
+     getBody window #+ [column [pure canvas,
+                                pure formula, 
+                                pure draw, 
+                                pure zoom, 
+                                pure zoomButton, 
+                                pure differentiate]]
 
      -- Styling
      getBody window # set style [("backgroundColor","lightblue"),
@@ -40,33 +41,36 @@ setup window =
      on valueChange' input $ \ _ -> readAndDraw input zoomInput canvas
 
      -- zoom and differentiate interactions --
-     on UI.click     zoom $ \ _ -> readAndDraw input zoomInput canvas 
-     on valueChange' zoomInput $ \ _ -> readAndDraw input zoomInput canvas
+     on UI.click     zoomButton    $ \ _ -> readAndDraw input zoomInput canvas 
+     on valueChange' zoomInput     $ \ _ -> readAndDraw input zoomInput canvas
      on UI.click     differentiate $ \ _ -> differentiateAndDraw input zoomInput canvas
-
 
 --------------- H ---------------
 
+-- Return all points that will be drawn.
 points :: Expr -> Double -> (Int, Int) -> [Point]
-points exp scale (width, height) = map (point_conversion) [(x, eval exp x) | x <- [-range, (-range+scale) .. range]]
+points exp scale (width, height) = 
+    map (point_conversion) [(x, eval exp x) | x <- [-range, (-range+scale) .. range]]
     where
-        point_conversion (x, y) = (x*(1/scale) + (fromIntegral width/2), y*(-1/scale) + (fromIntegral height/2))
+        -- point_conversion will convert a point in mathematical coordinates
+        -- to the canvas coordinates. 
+        point_conversion (x, y) = (x_conversion x, y_conversion y)
+        x_conversion x          = x*(1/scale) + (fromIntegral width/2)
+        y_conversion y          = y*(-1/scale) + (fromIntegral height/2)
         range                   = (scale * (fromIntegral width))/2
-
-
 
 --------------- I ---------------
 
 readAndDraw :: Element -> Element -> Canvas -> UI ()
 readAndDraw input zoomInput canvas =
-  do -- Get the current formula (a String) from the input element
-     formula <- get value input
-     zoomP <- get value zoomInput
+  do 
+     formula    <- get value input
+     zoomFactor <- get value zoomInput
      clearCanvas canvas
      let exp = readExpr formula
      case exp of 
         Just e -> do
-            let expPoints = points e (0.04 / (read zoomP)) (300, 300)
+            let expPoints = points e (0.04 / (read zoomFactor)) (canWidth, canHeight)
             path "blue" expPoints canvas
         Nothing -> 
             return ()
@@ -76,7 +80,6 @@ readAndDraw input zoomInput canvas =
 differentiateAndDraw :: Element -> Element -> Canvas -> UI ()
 differentiateAndDraw input zoomInput canvas = do 
     formula <- get value input
-    zoomP <- get value zoomInput
     let initialExpr = readExpr formula
     case initialExpr of 
         Just e -> do
--- lab4/Expr.hs
+++ lab4/Expr.hs
@@ -5,7 +5,6 @@ import Data.Maybe
 import Parsing
 import Test.QuickCheck
 
-
 --------------- A --------------- 
 
 -- New functions can be added here.
@@ -20,6 +19,7 @@ funStr :: Func -> String
 funStr Sin = "sin"
 funStr Cos = "cos"
 
+-- A dictionary can be used when there are more functions.
 getFunByName :: String -> Maybe Func
 getFunByName s | s == "sin" = Just Sin
                | s == "cos" = Just Cos
@@ -27,8 +27,7 @@ getFunByName s | s == "sin" = Just Sin
 
 data Expr = Num Double |
             Var |
-            Add Expr Expr |
-            Mul Expr Expr |
+            Bin Expr Char Expr |
             App Func Expr
             deriving (Read, Show, Eq)
 
@@ -39,42 +38,51 @@ num :: Double -> Expr
 num d = Num d
 
 add, mul :: Expr -> Expr -> Expr
-add e1 e2 = Add e1 e2
-mul e1 e2 = Mul e1 e2
+add e1 e2 = Bin e1 '+' e2
+mul e1 e2 = Bin e1 '*' e2
 
 sin, cos :: Expr -> Expr
 sin e = App Sin e
 cos e = App Cos e
 
+-- We thought about using the operator function inside the definition of Expr,
+-- but then we cannot derive Eq, which is needed when comparing Exprs.
+getOp :: Char -> (Double -> Double -> Double)
+getOp '+' = (+)
+getOp '*' = (*)
+
 --------------- B --------------- 
 
 showFactor :: Expr -> String
-showFactor (Add e1 e2) = "(" ++ showExpr (Add e1 e2) ++ ")"
-showFactor e           = showExpr e
+showFactor (Bin e1 '+' e2) = "(" ++ showExpr (add e1 e2) ++ ")"
+showFactor e               = showExpr e
 
 showExpr :: Expr -> String
-showExpr (Num n) | n >= 0 = show n 
+showExpr (Num n) | n >= 0    = show n 
                  | otherwise = "(" ++ show n ++ ")"
+showExpr Var                 = "x"
 
-showExpr Var     = "x"
-
-showExpr (Add e1 (Add e2 e3)) = showExpr e1 ++ "+(" ++ showExpr (Add e2 e3) ++ ")" 
-showExpr (Add e1 e2)          = showExpr e1 ++ "+" ++ showExpr e2
-
-showExpr (Mul e1 (Mul e2 e3)) = showFactor e1 ++ "*(" ++ showExpr (Mul e2 e3) ++ ")"
-showExpr (Mul e1 e2) = showFactor e1 ++ "*" ++ showFactor e2
-
-showExpr (App f (Add e1 e2))       = funStr f ++"(" ++ showExpr (Add e1 e2) ++ ")" 
-showExpr (App f (Mul e1 e2))       = funStr f ++"(" ++ showExpr (Mul e1 e2) ++ ")" 
-showExpr (App f e)    = funStr f ++ " " ++ showExpr e 
+showExpr (Bin e1 opl (Bin e2 opr e3)) 
+    | opl == opr && opl == '*'  = showFactor e1 ++ 
+                                  (opl:"(") ++ 
+                                  showFactor (Bin e2 opr e3) ++ 
+                                  ")" 
+    | opl == opr                = showExpr e1 ++ 
+                                  (opl:"(") ++ 
+                                  showExpr (Bin e2 opr e3) ++ 
+                                  ")" 
+showExpr (Bin e1 '*' e2)        = showFactor e1 ++ "*" ++ showFactor e2
+showExpr (Bin e1 op e2)         = showExpr e1 ++ [op] ++ showExpr e2
+showExpr (App f (Bin e1 op e2)) = funStr f ++ "(" ++ showExpr (Bin e1 op e2) ++ ")" 
+showExpr (App f e)              = funStr f ++ " " ++ showExpr e 
 
 --------------- C --------------- 
 
 eval :: Expr -> Double -> Double
 eval (Num n) _     = n
 eval Var     v     = v
-eval (Add e1 e2) v = eval e1 v + eval e2 v
-eval (Mul e1 e2) v = eval e1 v * eval e2 v
+eval (Bin e1 '+' e2) v = eval e1 v + eval e2 v
+eval (Bin e1 '*' e2) v = eval e1 v * eval e2 v
 eval (App f e)   v = evalFun f $ eval e v
 
 --------------- D --------------- 
@@ -115,10 +123,9 @@ paren = do
     skipSpace
     return exp
     
-
 expr, term, factor :: Parser Expr
-expr   = foldl1 Add <$> chain term (char '+')
-term   = foldl1 Mul <$> chain factor (char '*')
+expr   = foldl1 add <$> chain term (char '+')
+term   = foldl1 mul <$> chain factor (char '*')
 factor = funcP <|> varP <|> number <|> paren  
 
 readExpr :: String -> Maybe Expr
@@ -137,7 +144,7 @@ arbExpr i = frequency [(1, rNum), (1, rVar), (i, rBin), (i, rApp)]
         rNum = Num <$> arbitrary
         rVar = return Var
         rBin = do
-            op <- elements [Add, Mul]
+            op <- elements [add, mul]
             let i' = i `div` 2
             e1 <- arbExpr i'
             e2 <- arbExpr i'
@@ -158,45 +165,37 @@ simplify (Num d)   = Num d
 simplify Var       = Var
 simplify (App f e) = App f (simplify e)
 
-simplify (Add (Num d1) (Num d2)) = Num (d1 + d2)
-simplify (Add (Num 0) e)         = simplify e
-simplify (Add e (Num 0))         = simplify e
+simplify (Bin (Num d1) op (Num d2)) = Num $ (getOp op) d1 d2
+simplify (Bin (Num 0) '+' e)         = simplify e
+simplify (Bin e '+' (Num 0))         = simplify e
+simplify (Bin (Num 0) '*' e)         = Num 0
+simplify (Bin e '*' (Num 0))         = Num 0
+simplify (Bin (Num 1) '*' e)         = simplify e
+simplify (Bin e '*' (Num 1))         = simplify e
 
 -- Simplify "1 + (2 + x)" and similar cases to "3 + x".
-simplify (Add (Add (Num d1) e) (Num d2)) = simplify $ Add (Num (d1+d2)) e
-simplify (Add (Add e (Num d1)) (Num d2)) = simplify $ Add (Num (d1+d2)) e
-simplify (Add (Num d1) (Add e (Num d2))) = simplify $ Add (Num (d1+d2)) e
-simplify (Add (Num d1) (Add (Num d2) e)) = simplify $ Add (Num (d1+d2)) e
+-- Some copy and paste code here but there doesn't seem to be a good way to refactor this
+simplify (Bin (Bin (Num d1) op1 e) op2 (Num d2)) 
+    | op1 == op2 = simplify $ Bin (Num ((getOp op1) d1 d2)) op1 e
+simplify (Bin (Bin e op1 (Num d1)) op2 (Num d2)) 
+    | op1 == op2 = simplify $ Bin (Num ((getOp op1) d1 d2)) op1 e
+simplify (Bin (Num d1) op1 (Bin e op2 (Num d2))) 
+    | op1 == op2 = simplify $ Bin (Num ((getOp op1) d1 d2)) op1 e
+simplify (Bin (Num d1) op1 (Bin (Num d2) op2 e)) 
+    | op1 == op2 = simplify $ Bin (Num ((getOp op1) d1 d2)) op1 e
 
 -- Need to prevent infinite recurisive call.
-simplify (Add e1 e2) = if e1 == e1' && e2 == e2' 
-                         then Add e1' e2'
-                         else simplify $ Add e1' e2'
+simplify (Bin e1 op e2) = if e1 == e1' && e2 == e2' 
+                          then Bin e1' op e2'
+                          else simplify $ Bin e1' op e2'
     where e1' = simplify e1
           e2' = simplify e2
-
-
-simplify (Mul (Num d1) (Num d2)) = Num (d1 * d2)
-simplify (Mul (Num 0) e)         = Num 0
-simplify (Mul e (Num 0))         = Num 0
-simplify (Mul (Num 1) e)         = simplify e
-simplify (Mul e (Num 1))         = simplify e
-simplify (Mul (Mul (Num d1) e) (Num d2)) = simplify $ Mul (Num (d1*d2)) e
-simplify (Mul (Mul e (Num d1)) (Num d2)) = simplify $ Mul (Num (d1*d2)) e
-simplify (Mul (Num d1) (Mul e (Num d2))) = simplify $ Mul (Num (d1*d2)) e
-simplify (Mul (Num d1) (Mul (Num d2) e)) = simplify $ Mul (Num (d1*d2)) e
-simplify (Mul e1 e2) = if e1 == e1' && e2 == e2' 
-                         then Mul e1' e2'
-                         else simplify $ Mul e1' e2'
-    where e1' = simplify e1
-          e2' = simplify e2
-
 
 prop_simplify :: Expr -> Double -> Property 
 prop_simplify e x =  within timeout $ diff < epsilon && simplify se == se  
     where se = simplify e
           timeout = 1000000
-          epsilon = 0.00001
+          epsilon = 0.0001
           diff    = abs (eval e x - eval se x)
 
 --------------- G --------------- 
@@ -204,7 +203,11 @@ prop_simplify e x =  within timeout $ diff < epsilon && simplify se == se
 differentiate :: Expr -> Expr
 differentiate (Num d)     = Num 0
 differentiate Var         = Num 1
-differentiate (Add e1 e2) = simplify (Add (differentiate e1) (differentiate e2))
-differentiate (Mul e1 e2) = simplify (Add (Mul (differentiate e1) e2) (Mul (differentiate e2) e1))
-differentiate (App Sin e) = simplify (Mul(differentiate e) (App Cos e))
-differentiate (App Cos e) = simplify (Mul (Num (-1)) (Mul(differentiate e) (App Sin e)))
+differentiate (Bin e1 '+' e2) = simplify $ add (differentiate e1) 
+                                               (differentiate e2)
+differentiate (Bin e1 '*' e2) = simplify $ add (mul (differentiate e1) e2)
+                                               (mul (differentiate e2) e1)
+differentiate (App Sin e) = simplify (mul (differentiate e) (App Cos e))
+differentiate (App Cos e) = simplify (mul (Num (-1)) 
+                                          (mul (differentiate e) (App Sin e)))
+
